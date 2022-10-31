272069e4238e620a39f93bd88b1731fd32ef29c8
Filippo <102758065+ErScatenato@users.noreply.github.com>
2022-10-31 10:40:11 +0100

    Add files via upload

---
+++ C05/ex00/ft_iterative_factorial.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fcorvaro <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/31 10:31:04 by fcorvaro          #+#    #+#             */
+/*   Updated: 2022/10/31 10:39:20 by fcorvaro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+/*#include <stdio.h>*/
+
+int	ft_iterative_factorial(int nb)
+{
+	unsigned int	i;
+
+	i = 1;
+	if (nb < 0)
+		return (0);
+	while (nb > 0)
+	{
+		i *= nb;
+		nb--;
+	}
+	return (i);
+}
+/*
+int	main()
+{
+	printf("%d\n", ft_iterative_factorial(-1));
+	printf("%d\n", ft_iterative_factorial(0));
+	printf("%d\n", ft_iterative_factorial(10));
+	return (0);
+}*/
