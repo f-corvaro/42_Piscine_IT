c1eb724b482e59ce12e3764a9491d45dd6078217
Filippo Corvaro <fcorvaro@e3r10p5.42roma.it>
2022-10-31 17:53:19 +0100

    add ex03

--- C05/ex03/ft_recursive_power.c
+++ C05/ex03/ft_recursive_power.c
@@ -5,19 +5,25 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: fcorvaro <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/31 13:45:18 by fcorvaro          #+#    #+#             */
-/*   Updated: 2022/10/31 15:13:48 by fcorvaro         ###   ########.fr       */
+/*   Created: 2022/10/31 17:45:04 by fcorvaro          #+#    #+#             */
+/*   Updated: 2022/10/31 17:49:24 by fcorvaro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <stdio.h>
-
-int	ft_recurive_power(int nb, int power)
+/*#include <stdio.h>*/
+int	ft_recursive_power(int nb, int power)
 {
 	if (power > 1)
-		nb *= ft_recursive_power(nb, power - 1
+	{
+		nb = nb * ft_recursive_power(nb, power - 1);
+	}
+	if (power < 0)
+		return (0);
+	if (power == 0)
+		return (1);
+	return (nb);
 }
-
-int	main()
+/*
+int main()
 {
-
-}
+	printf("%d", ft_recursive_power(3,-2));
+}*/
