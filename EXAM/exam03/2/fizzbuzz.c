03dc8eb33ea90179e932cd8b4d94ca331b7199f4
Filippo Corvaro <fcorvaro@e3r6p8.42roma.it>
2022-11-01 13:00:12 +0100

    add

--- EXAM/exam03/2/fizzbuzz.c
+++ EXAM/exam03/2/fizzbuzz.c
@@ -1,2 +1,52 @@
 #include <unistd.h>
 
+void	printnb(int nb)
+{
+	char	s;
+	char	p;
+	int		i;
+	int		j;
+
+	i = nb / 10;
+	s = i + 48;
+	j = nb % 10;
+	p = j + 48;
+	write(1, &s, 1);
+	write(1, &p, 1);
+	write(1, "\n", 1);
+}
+
+void	fizzbuzz(void)
+{
+	int		numb;
+	char	car;
+
+	numb = 1;
+	while (numb <= 20)
+	{
+		if ((numb % 3) == 0 && (numb % 5) == 0)
+			write(1, "fizzbuzz\n", 9);
+		else if ((numb % 3) == 0)
+			write(1, "fizz\n", 5);
+		else if ((numb % 5) == 0)
+			write(1, "buzz\n", 5);
+		else
+		{
+			if (numb <= 9)
+			{
+				car = numb + 48;
+				write(1, &car, 1);
+				write(1, "\n", 1);
+			}
+			else
+				printnb(numb);
+		}
+		numb++;
+	}
+}
+
+int	main(void)
+{
+	fizzbuzz();
+	return (0);
+}
