c5bf62853d342f6caaac6164614bc85d8c5637fa
Filippo Corvaro <fcorvaro@e3r6p8.42roma.it>
2022-11-02 13:46:59 +0100

    add

---
+++ EXAM/exam03/8/ft_atoi.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int	ft_atoi(const char *str)
+{
+	int	i;
+	int	result;
+	int	sign;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	else if (str[i] == '+')
+		i++;
+	while (str[i] && str[i] >= '0' && str[i] <= '9')
+	{
+		result *= 10;
+		result += str[i] - 48;
+		i++;
+	}
+	return (result * sign);
+}
+
+int	main()
+{
+	char	nb[] = "-+123";
+
+	printf("%d", ft_atoi(nb));
+	return (0);
+}
