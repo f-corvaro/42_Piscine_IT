90f91843f800149ed090ebcca5f56d06484335f0
Filippo <102758065+ErScatenato@users.noreply.github.com>
2022-11-02 10:04:16 +0100

    Add files via upload

---
+++ EXAM/exam03/2/fizzbuzz2.c
@@ -0,0 +1,41 @@
+#include <unistd.h>
+
+void	ft_fizzbuzz(void)
+{
+	int		i;
+	char	c;
+
+	i = 1;
+	while (i <= 100)
+	{
+		if (i % 3 == 0)
+			write(1, "fizz\n", 5);
+		else if (i % 5 == 0)
+			write(1, "buzz\n", 5);
+		else if ((i % 5 == 0) && (i % 3 == 0))
+			write(1, "fizzbuzz\n", 9);
+		else 
+		{
+			if ( i <= 9)
+			{
+				c = i + 48;
+				write(1, &c, 1);
+			}
+			else
+			{
+				c = (i / 10) + 48;
+				write(1, &c, 1);
+				c = (i % 10) + 48;
+				write(1, &c, 1);
+			}
+			write(1, "\n", 1);
+		}
+		i++;
+	}
+}
+
+int	main()
+{
+	ft_fizzbuzz();
+	return (0);
+}
