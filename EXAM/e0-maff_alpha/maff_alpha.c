5bafa831a84ce18ddc66878dc7f6d1e112173a14
Filippo Corvaro <fcorvaro@e3r6p8.42roma.it>
2022-11-02 17:38:54 +0100

    add

---
+++ EXAM/e0-maff_alpha/maff_alpha.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+
+int	main()
+{
+	char	c1;
+	char	c2;
+
+	c1 = 'a';
+	c2 = 'A';
+	
+	while (c1 <= 'z' && c2 <= 'Z')
+	{
+		if ((c1 % 2) != 0)
+		{
+			write(1, &c1, 1);
+		}
+		else
+		{
+			write(1, &c2, 1);
+		}
+		c1++;
+		c2++;
+	}
+	write(1, "\n", 1);
+	return (0);
+}
