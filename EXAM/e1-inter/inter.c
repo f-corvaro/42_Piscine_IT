dcc2923ab1ae82393b21c67dc37961f956135ee8
Filippo Corvaro <fcorvaro@e3r6p8.42roma.it>
2022-11-03 11:36:58 +0100

    add

---
+++ EXAM/e1-inter/inter.c
@@ -0,0 +1,29 @@
+#include <unistd.h>
+int	check(char *str, char c, int len)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] && (i < len || len == -1))
+		if (str[i++] == c)
+			return (1);
+	return (0);
+}
+
+int	main(int ac, char **av)
+{
+	int	i;
+
+	i = 0;
+	if (ac == 3)
+	{
+		while (av[1][i])
+		{
+			if (!check(av[1], av[1][i], i) && check(av[2], av[1][i], -1))
+				write(1, &av[1][i], 1);
+			i++;
+		}
+	}
+	write(1, "\n", 1);
+	return (0);
+}
