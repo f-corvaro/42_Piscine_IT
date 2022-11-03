49bdf500c9d3fb619489b501c90dd2cf7e944689
Filippo Corvaro <fcorvaro@e3r6p8.42roma.it>
2022-11-03 14:32:54 +0100

    add

---
+++ EXAM/e1-wdmatch/wdmatch.c
@@ -0,0 +1,54 @@
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+int	main(int ac, char **av)
+{
+	int	i;
+	int	j;
+	int	len;
+
+	i = 0;
+	j = 0;
+	len = 0;
+	if (ac == 3)
+	{
+		while (av[1][i])
+		{
+			while (av[2][j])
+			{
+				if (av[1][i] == av[2][j])
+				{
+					len++;
+					break ;
+				}
+				j++;
+			}
+			i++;
+		}
+		if (len == ft_strlen(av[1]))
+			ft_putstr(av[1]);
+	}
+	write(1, "\n", 1);
+	return (0);
+}
